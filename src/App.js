1c88618bf79c1da08799971d81b737ce2acc62b9
Renato Ribeiro Altoe <renatoribeiroalto@gmail.com>
2022-07-07 18:14:57 -0300

    envio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWord from './components/HelloWord'
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
+
+  const name='Renato'
+
+  // function sum(){}
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SayMyName nome="Heisenberg"/>
+      <Pessoa nome="Renato" idade="19" profissao="Estudante" foto="https://via.placeholder.com/150"/>
     </div>
   );
 }
---
+++ src/components/SayMyName.js
@@ -0,0 +1,9 @@
+function SayMyName(props){
+    return(
+        <div>
+            <p>Diga aí {props.nome}</p>
+        </div>
+    )
+}
+
+export default SayMyName
